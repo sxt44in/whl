264cb5dda7fb6f743bc8c89c1c429f4fb9b572b0
sxt44in <sebastian.raphel@gmail.com>
2019-02-07 23:56:00 -0600

    test xyn

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# declare STRING variable
+string="Hello World new "
+#print variable on a screen
+echo $string
+
+
+#for i in $(cat test.txt); do
+#            echo $i  
+#        done
+
+
+awk '{ print }'  test.txt
